41f1280f8209defd657e9fd34224ba74a130e5a8
Desarrollador Iglu <n3f73r@gmail.com>
2023-09-04 17:48:18 -0500

    Visibilidad OXXO funcional (faltan mas prubas)

--- src/js/modulos/modulo3.js
+++ src/js/modulos/modulo3.js
@@ -1,16 +1,13 @@
 import * as CONSTANTS from'../constants/constants';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
-import { Preferences } from '@capacitor/preferences';
 import { Haptics, ImpactStyle } from '@capacitor/haptics';
 import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
   
 // Elems 
-let pdv = document.getElementById('pdv');
 let visibilidad = document.getElementById('visibilidad');
 let tipo_visibilidad = document.getElementById('tipo_visibilidad');
 let visibilidad_competencia = document.getElementById('visibilidad_competencia');
 let tipo_visibilidad_competencia = document.getElementById('tipo_visibilidad_competencia');
-let num_ventas_competencia = document.getElementById('num_ventas_competencia');
 let foto_visibilidad_marca = document.getElementById('foto_visibilidad_marca');
 let foto_visibilidad_competencia = document.getElementById('foto_visibilidad_competencia');
 
@@ -18,7 +15,10 @@ let foto_visibilidad_competencia = document.getElementById('foto_visibilidad_com
 let foto_visibilidad_marcaBox = document.getElementById('foto_visibilidad_marcaBox');
 let foto_visibilidad_competenciaBox = document.getElementById('foto_visibilidad_competenciaBox');
 
+let allowPhotos = true;
+
 // BUTTONS
+let switchOxxo = document.getElementById('switch-oxxo');
 let btnStore = document.getElementById('store');
 let btnReset = document.getElementById('reset');
 let btnVolver = document.getElementById('volver'); 
@@ -31,26 +31,32 @@ function mount(){
 }
 
 async function store (){ 
-    if (validation()){
-        let dataModulo = [{
-            visibilidad: visibilidad.value,
-            tipo_visibilidad: tipo_visibilidad.value,
-            visibilidad_competencia: visibilidad_competencia.value,
-            tipo_visibilidad_competencia: tipo_visibilidad_competencia.value,
-            foto_visibilidad_marca: foto_visibilidad_marca.src,
-            foto_visibilidad_competencia: foto_visibilidad_competencia.src,
-        }]; 
-        
-        // await Preferences.set({ key: STORAGE_KEY, value: JSON.stringify({path: STORAGE_PATH}) });
-        appendData(CONSTANTS.STORAGE_PATHM3, dataModulo);
-
-        // await Filesystem.deleteFile({
-        //     path: 'secrets/photos.txt',
-        //     directory: Directory.Documents,
-        // });
+    let dataModulo = []; 
+    if (switchOxxo.checked){
+        dataModulo = [{
+            visibilidad: null,
+            tipo_visibilidad: null,
+            visibilidad_competencia: null,
+            tipo_visibilidad_competencia: null,
+            foto_visibilidad_marca: null,
+            foto_visibilidad_competencia: null,
+        }];                     
     }else {
-        alert("Debes rellenar todos los campos");
+        if (validation()){
+            dataModulo = [{
+                visibilidad: visibilidad.value,
+                tipo_visibilidad: tipo_visibilidad.value,
+                visibilidad_competencia: visibilidad_competencia.value,
+                tipo_visibilidad_competencia: tipo_visibilidad_competencia.value,
+                foto_visibilidad_marca: foto_visibilidad_marca.src,
+                foto_visibilidad_competencia: foto_visibilidad_competencia.src,
+
+            }];
+        }else {
+            alert("Debes rellenar todos los campos"); 
+        }
     }
+    appendData(CONSTANTS.STORAGE_PATHM3, dataModulo);
 }
 
 async function appendData(src, data){
@@ -72,33 +78,37 @@ async function appendData(src, data){
 }
 
 const visibilidad_marcaPicture = async () => {
-    const image = await Camera.getPhoto({
-        quality: CONSTANTS.QUALITY,
-        allowEditing: false,
-        source: CameraSource.Camera,
-        resultType: CameraResultType.Base64
-    }); 
-
-    var image64 = image.base64String;
-
-    foto_visibilidad_marca.src = `data:image/png;base64,${image64}`;
-    foto_visibilidad_marca.style.display = "block";
-    foto_visibilidad_marcaBox.style.display = "none";
+    if (allowPhotos){
+        const image = await Camera.getPhoto({
+            quality: CONSTANTS.QUALITY,
+            allowEditing: false,
+            source: CameraSource.Camera,
+            resultType: CameraResultType.Base64
+        }); 
+    
+        var image64 = image.base64String;
+    
+        foto_visibilidad_marca.src = `data:image/png;base64,${image64}`;
+        foto_visibilidad_marca.style.display = "block";
+        foto_visibilidad_marcaBox.style.display = "none";
+    }
 };
 
 const foto_visibilidad_competenciaPicture = async () => {
-    const image = await Camera.getPhoto({
-        quality: CONSTANTS.QUALITY,
-        allowEditing: false,
-        source: CameraSource.Camera,
-        resultType: CameraResultType.Base64
-    }); 
-
-    var image64 = image.base64String;
-
-    foto_visibilidad_competencia.src = `data:image/png;base64,${image64}`;
-    foto_visibilidad_competencia.style.display = "block";
-    foto_visibilidad_competenciaBox.style.display = "none";
+    if (allowPhotos){
+        const image = await Camera.getPhoto({
+            quality: CONSTANTS.QUALITY,
+            allowEditing: false,
+            source: CameraSource.Camera,
+            resultType: CameraResultType.Base64
+        }); 
+    
+        var image64 = image.base64String;
+    
+        foto_visibilidad_competencia.src = `data:image/png;base64,${image64}`;
+        foto_visibilidad_competencia.style.display = "block";
+        foto_visibilidad_competenciaBox.style.display = "none";
+    }
 };
 
 function validation (){
@@ -121,6 +131,23 @@ function validation (){
  
 async function vibrate(){
     await Haptics.vibrate();
+}
+
+function disableFileds(){
+    reset();
+    if (switchOxxo.checked){
+        elems.forEach((elem) => {
+            elem.disabled  = true;
+        });
+    
+        allowPhotos = !allowPhotos;
+    }else {
+        elems.forEach((elem) => {
+            elem.disabled  = false;
+        });
+    
+        allowPhotos = !allowPhotos;
+    }
 }
  
 function reset(){ 
@@ -148,13 +175,14 @@ function fillFields(){
 }
 
 window.addEventListener("DOMContentLoaded", function() {
-    mount()
+    mount();
 });
 
 // Events
 btnStore.addEventListener('click', store);
 btnReset.addEventListener('click', reset);
 btnVolver.addEventListener('click', volver);
+switchOxxo.addEventListener('change', disableFileds);
 
 foto_visibilidad_marcaBox.addEventListener('click', visibilidad_marcaPicture);
 foto_visibilidad_competenciaBox.addEventListener('click', foto_visibilidad_competenciaPicture);
